abbc4e0ef37bb0a5db54d427fc7e895e53955753
Julius Busecke <julius@ldeo.columbia.edu>
2024-06-24 23:11:31 +0000

    Started postprocessing module + prototype eval plots

--- ocean_emulators/dummy.py
+++
@@ -1,5 +0,0 @@
-"""Just a dummy module to test the package structure."""
-
-
-def add(a, b):
-    return a + b
---
+++ tests/test_postprocessing.py
@@ -0,0 +1,9 @@
+import xarray as xr
+import numpy as np
+from ocean_emulators.postprocessing import post_processor
+
+def test_post_processor():
+    ds_raw = xr.DataArray(np.random.random([3,180, 360, 77]), dims=['time', 'y','x', 'var']).to_dataset(name='__xarray_dataarray_variable__')
+    ds = post_processor(ds_raw)
+    assert set(ds.data_vars) == set(['so', 'thetao', 'zos', 'uo', 'vo'])
+    assert ds.sizes == {'time':3, 'x':360, 'y':180, 'lev':19}
